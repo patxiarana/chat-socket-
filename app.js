43d2b52797de6cb14e2c2725a77b34d75070ccb5
patxi <patxiarana05@gmail.com>
2023-01-15 09:42:23 -0300

    finish proyect

--- app.js
+++ app.js
@@ -1,9 +1,20 @@
-
 const express = require('express')
 const app = express()
 
 const http = require('http')
 const server = http.createServer(app)
+
+const {Server} = require('socket.io')
+const io = new Server(server)
+
+  io.on('connection', (socket) => {
+   //console.log('un usuario se a conectado')
+   /*socket.on('chat' , (msg) => {
+    console.log('mensaje:'  +msg) }) */
+    socket.on('chat', (msg) => {
+        io.emit('chat', msg)
+    })
+})
 
 app.get('/', (req, res)=>{
 //res.send('<h1>aplicacion chat</h1>')
